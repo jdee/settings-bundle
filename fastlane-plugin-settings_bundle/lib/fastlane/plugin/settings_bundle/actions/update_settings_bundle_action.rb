bc4f95deb83efda6cc97f6084fc687f5198a345c
Jimmy Dee <Jimmy.Dee@bodybuilding.com>
2016-11-09 16:40:40 -0800

    more params

--- fastlane-plugin-settings_bundle/lib/fastlane/plugin/settings_bundle/actions/update_settings_bundle_action.rb
+++ fastlane-plugin-settings_bundle/lib/fastlane/plugin/settings_bundle/actions/update_settings_bundle_action.rb
@@ -5,17 +5,17 @@ module Fastlane
     class UpdateSettingsBundleAction < Action
       def self.run(params)
         key = params[:key]
+        configuration = params[:configuration]
+        file = params[:file]
 
         # try to open project file (raises)
         project = Xcodeproj::Project.open params[:xcodeproj]
 
         helper = Helper::SettingsBundleHelper
 
-        # TODO: Make configuration a parameter
-        current_app_version = helper.formatted_version_from_info_plist project, "Release"
+        current_app_version = helper.formatted_version_from_info_plist project, configuration
 
-        # TODO: Make file a parameter
-        helper.update_root_plist project, "Root.plist", key, current_app_version
+        helper.update_root_plist project, file, key, current_app_version
       rescue => e
         UI.user_error! e.message
       end
@@ -44,6 +44,18 @@ module Fastlane
                                   env_name: "SETTINGS_BUNDLE_KEY",
                                description: "The user defaults key to update in the settings bundle",
                                   optional: false,
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :configuration,
+                                  env_name: "SETTINGS_BUNDLE_CONFIGURATION",
+                               description: "The build configuration to use for the Info.plist file",
+                                  optional: true,
+                             default_value: "Release",
+                                      type: String),
+          FastlaneCore::ConfigItem.new(key: :file,
+                                  env_name: "SETTINGS_BUNDLE_FILE",
+                               description: "The plist file in the Settings.bundle to update",
+                                  optional: true,
+                             default_value: "Root.plist",
                                       type: String)
         ]
       end
