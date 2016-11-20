cd7212a5a1052253d49b4324c25acff12274a344
Jimmy Dee <Jimmy.Dee@bodybuilding.com>
2016-11-20 14:28:17 -0800

    simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
+require 'simplecov'
+
+SimpleCov.start
+
 # This module is only used to check the environment is currently a testing env
 module SpecHelper
 end
