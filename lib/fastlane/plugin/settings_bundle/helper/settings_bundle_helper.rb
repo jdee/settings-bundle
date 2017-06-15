bd01f1672d0bdc17a0083a946559f308cc6ff627
Jimmy Dee <jgvdthree@gmail.com>
2017-06-14 18:58:10 -0700

    copyright update

--- lib/fastlane/plugin/settings_bundle/helper/settings_bundle_helper.rb
+++ lib/fastlane/plugin/settings_bundle/helper/settings_bundle_helper.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 Jimmy Dee
+# Copyright (c) 2016-17 Jimmy Dee
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
